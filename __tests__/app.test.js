fd7e9e5d918bba95c286eee3bb6ff278bbfab558
Cody Brown <cody6lefever@gmail.com>
2020-03-17 13:27:37 -0700

    finding what tests are needed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Recipe = require('../lib/models/Recipe');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -17,4 +18,128 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  //post
+  it('creates a recipe', () => {
+    return request(app)
+      .post('/api/v1/recipes')
+      .send({
+        name: 'cookies',
+        directions: [
+          'preheat oven to 375',
+          'mix ingredients',
+          'put dough on cookie sheet',
+          'bake for 10 minutes'
+        ]
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ],
+          __v: 0
+        });
+      });
+  });
+
+  //get 1of2
+  it('gets all recipes', async() => {
+    const recipes = await Recipe.create([
+      { name: 'cookies', directions: [] },
+      { name: 'cake', directions: [] },
+      { name: 'pie', directions: [] }
+    ]);
+
+    return request(app)
+      .get('/api/v1/recipes')
+      .then(res => {
+        recipes.forEach(recipe => {
+          expect(res.body).toContainEqual({
+            _id: recipe._id.toString(),
+            name: recipe.name
+          });
+        });
+      });
+  });
+
+  //get 2of2 needs work
+  it('gets all recipes', async() => {
+    const recipes = await Recipe.create([
+      { name: 'cookies', directions: [] },
+      { name: 'cake', directions: [] },
+      { name: 'pie', directions: [] }
+    ]);
+
+    return request(app)
+      .get('/api/v1/recipes')
+      .then(res => {
+        recipes.forEach(recipe => {
+          expect(res.body).toContainEqual({
+            _id: recipe._id.toString(),
+            name: recipe.name
+          });
+        });
+      });
+  });
+
+  //patch
+  it('updates a recipe by id', async() => {
+    const recipe = await Recipe.create({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+
+    return request(app)
+      .patch(`/api/v1/recipes/${recipe._id}`)
+      .send({ name: 'good cookies' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'good cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ],
+          __v: 0
+        });
+      });
+  });
+
+  //delete needs work
+  it('deletes a recipe by id', async() => {
+    const recipe = await Recipe.create({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+
+    return request(app)
+      .delete(`/api/v1/recipes/${recipe._id}`)
+      .send({ name: 'good cookies' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: null,
+          name: null,
+          directions: null,
+          __v: 0
+        });
+      });
+  });
+
 });
